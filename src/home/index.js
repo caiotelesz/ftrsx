d8ccb3707d62c43b3a7ac3208df2124b1ae70506
eryczh <ericandrade2810@gmail.com>
2024-04-17 17:14:48 -0300

    iniciando page home

--- src/home/index.js
+++ src/home/index.js
@@ -1,21 +1,37 @@
-import './index.scss';
-import { Link } from 'react-router-dom'
+import "./index.scss";
+import { Link } from "react-router-dom";
 
 import Logo from "../components/images/logo-home.png";
+import Logoheader from "../components/images/logo-header.png";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <img className='image-home' src={Logo} alt="Logo-Home"/>
+      <header className>
+        <div className="container">
+          <div className="logo">
+            <Link to="/">
+              <img className="logo-img" src={Logoheader} alt="Logo" />
+            </Link>
+          </div>
 
-        <Link to='/vitrine'> vitrine </Link>
-        <Link to='/contato'> contato </Link>
-
-        <h1>FTRSX</h1>
-
+          <div className="menu">
+            <nav>
+              <a href="/vitrine">shop all</a>
+              <a href="/">home</a>
+              <a href="/contato">contato</a>
+              <a href="https://www.instagram.com/featuresx/">Instagram</a>
+            </nav>
+          </div>
+          <div className="icones"></div>
+        </div>
       </header>
+
+      
+        <Link to="/vitrine">
+          <img className="image-home" src={Logo} alt="Logo-Home" />
+        </Link>
+  
     </div>
   );
 }
