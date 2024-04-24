985a6d9be1d634ec1f0e5d169800b767f432566c
eryczh <ericandrade2810@gmail.com>
2024-04-24 19:54:19 -0300

    imagem atualizada, footer adicionado cadastro, login e vitrine

--- src/vitrine/index.js
+++ src/vitrine/index.js
@@ -51,10 +51,43 @@ export default function Vitrine () {
       
 
       <section className='secao2'>
-        <img src='/assets/images/frase.png' />
+        <div>
+        <img src='/assets/images/frase-recortada.png' alt='frase'/>
+        </div>
+      </section>
+
+      <section className='secao3'>
+        <div>
+          
+        </div>
       </section>
       
-      <Link></Link>
+      <section className='page-footer'>
+        <div>
+          <Link to="/">
+            <img className='img-footer' src='/assets/images/logo-header.png' alt="header logo"/>
+          </Link>
+        </div>
+
+        <footer>
+          <div>
+            <p>contato@gmail.com</p>
+            <p>(11) 4002-8922 </p>
+            <p>CNPJ 00.000.000/0000-00</p>
+          </div>
+          
+          <a href='/'>
+            <p>Informações</p>
+          </a>
+          <a href='/'>
+            <p>Política de Privacidade</p>
+          </a>
+          <a href='/'>
+            <p>Termos de Uso</p>
+          </a>
+         
+        </footer>
+      </section>
       </>
   );
 }
