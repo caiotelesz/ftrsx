79354dfd0719ed853d74c72e0016d6ef829f940c
eryczh <ericandrade2810@gmail.com>
2024-04-29 21:24:00 -0300

    Co-authored-by: Caio Teles <caiotelesz@users.noreply.github.com>

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,15 +1,16 @@
 import './index.scss';
+import imagem from './teste.png';
 
-export default function Box () {
+export default function Box (props) {
 
     return (
     <>
         <div className='card'>
-            <img src='../../assets/images/teste.png'/>
+            <img src={props.imagem ?? imagem} alt='camiseta'/>
             <div>
-                <h1>lalalallala</h1>
+                <h1 className={`${props.nomeProduto}`}>{props.nome ?? 'camiseta'}</h1>
                 <h2>laklkaalal</h2>
-                <span>R$ preco</span>
+                <span>{props.preco ?? 'R$ 180.00'}</span>
             </div>
         </div>
     </>
