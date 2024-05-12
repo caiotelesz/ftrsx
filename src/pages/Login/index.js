2bd794cb1235f018ea9671ac9ef7d5a9cf2979d8
caiotelesz <caioteles1267@gmail.com>
2024-05-12 18:35:52 -0300

    método de logar conta

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import Cabecalho from '../../components/Cabacalho';
 import Footer from '../../components/Footer';
@@ -12,15 +12,20 @@ export default function Login() {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const navigate = useNavigate();
+    const params = useParams();
 
     async function logarConta() {
-      let url = 'http://localhost:5000/user_login';
-
       try {
-        let r = await axios.post(url, {email, senha});
+        let url = 'http://localhost:5000/user/login';
+        let r = await axios.post(url, {params: {email, senha}});
         let userData = r.data;
 
-        navigate(`/funcao_admin/${userData.userId}`);
+        if(userData && userData.loggedIn) {
+          navigate('/funcao_admin');
+        } else {
+          alert('Verifique se o email ou senha está certo');
+        }
+        
       } catch (error) {
         alert('Verifique se o email ou senha está certo');
       }
